d8e090af9912a2663a3a84fe3aafc0f54d402bec
kimsehwanestat <sktpghks138@gmail.com>
2020-09-27 23:40:14 +0900

    make storageManager in progress

--- backend_codes/greengrass/greengrasslambda/libs/storageManager.py
+++ backend_codes/greengrass/greengrasslambda/libs/storageManager.py
@@ -4,13 +4,14 @@ import csv
 import datetime
 import time
 import threading
+from .util import timestamp_to_datetime
 
 LOCAL_DATA_STORE_PATH = os.environ.get('LOCAL_DATA_STORE_PATH', '/rawcar/rawdata') # 원본 데이터를 저장할 디바이스 상의 경로.
 # 테스트 코드는 현재 경로에다 저장할래요.
 
 #TODO: 1. dataGetter 하는 클래스로부터 데이터를 전달 받고, profile 상의 필드와 데이터를 매칭시켜서 csv 파일로 저장
 
-# TODO: S3 에 1분간 저장된 원본 데이터를 저장하는 로직이 들어 갈 예정
+#TODO: S3 에 1분간 저장된 원본 데이터를 저장하는 로직이 들어 갈 예정
 DEVICE_ID = os.environ.get("AWS_IOT_THING_NAME", 'test_id')
 s3 = boto3.resource('s3')
 
@@ -22,6 +23,7 @@ class BaseStorageManager:
         self.device_id = device_id
         self.fields = []
         self.data  =  []
+        self.timestamp = None 
     
     def get_payload(self):
         return self.payload
@@ -41,11 +43,17 @@ class BaseStorageManager:
             self.reset_buffers()
             if self.check_payload(data):
                 self.payload = data
+                self.fields = self.payload.get('Fields')
+                self.data = self.payload.get('data')
+                self.timestamp = self.payload.get('timestamp')
             else:
                 print("error occured")
         else:
             if self.check_payload(data):
                 self.payload = data
+                self.fields = self.payload.get('Fields')
+                self.data = self.payload.get('data')
+                self.timestamp = self.payload.get('timestamp')
             else:
                 print("errror occured")
 
@@ -53,6 +61,18 @@ class BaseStorageManager:
         self.payload = {}
         self.fields = []
         self.data = []
+
+    def make_csv(self):
+        pass
+
+    #TODO: datetime 오브젝트에서, 기준이 되는 minutes를 정하고, 다음 minutes가 되기 전까지의
+    # 데이터를 배열로 저장, csv에 쓰면 될 듯 하다.
+    # 포멧
+    # 기준 필드명 필드명 필드명 필드명
+    # 시간   1   2    3    4
+    # 시간   9   0    2    1
+    # 시간   2   1    3    4
+    # 시간   4   2    1    3
 
 
 class LocalStorageManager(BaseStorageManager):
--- backend_codes/greengrass/greengrasslambda/libs/util.py
+++ backend_codes/greengrass/greengrasslambda/libs/util.py
@@ -1,8 +1,12 @@
 import os
+import time
+import datetime
 
 PUBLISH_TOPIC = os.environ.get('PUBLISH_TOPIC')
 
 def get_publish_topic():
     return PUBLISH_TOPIC
 
-    
+def timestamp_to_datetime(timestamp):
+    return datetime.datetime.fromtimestamp(timestamp)
+
