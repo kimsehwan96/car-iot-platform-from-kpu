6c3f099b3653d40fec88e3e43dfc8efbb64508ed
kimsehwanestat <sktpghks138@gmail.com>
2020-09-30 23:00:04 +0900

    remove unused java files

--- backend_codes/provisioning/foundation/CDDBaselineJava/src/main/java/com/awslabs/aws/iot/greengrass/cdd/helpers/TimerHelper.java
+++
@@ -1,8 +0,0 @@
-package com.awslabs.aws.iot.greengrass.cdd.helpers;
-
-import java.util.concurrent.ScheduledFuture;
-import java.util.concurrent.TimeUnit;
-
-public interface TimerHelper {
-    ScheduledFuture<?> scheduleRunnable(Runnable runnable, int startDelay, int period, TimeUnit timeUnit);
-}
