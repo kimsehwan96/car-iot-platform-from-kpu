c321f46a7a060d500e0a85d282f060db0849356a
kimsehwanestat <sktpghks138@gmail.com>
2020-10-03 16:11:29 +0900

    start to test for stat data from raw data in S3

--- backend_codes/lambda/lmd_get_trigger.py
+++ backend_codes/lambda/lmd_get_trigger.py
@@ -7,4 +7,5 @@ import boto3
 def hanlder(event, context):
     print("this lambda has been invoked !")
     print(event)
+
     
--- backend_codes/lambda/lmd_trigger.py
+++ backend_codes/lambda/lmd_trigger.py
@@ -6,8 +6,12 @@ import abc
 import os
 import datetime
 import time
+import json
 
 BUCKET_NAME = os.environ.get('RAW_BUCKET', 'if this texts show, Env var wasnt detectd')
+TRIGGERD_LAMDA = os.environ.get('TRIGGERD_LAMDA', 'batch-lambda-dev-stat-get-trigger')
+
+lmd = boto3.client('lambda')
 
 def timenow_dt():
     time_now = time.time()
@@ -22,5 +26,12 @@ def handler(event, context):
     #dt = timenow_dt()
     dt = datetime.datetime(2020,9, 29, 22, 20)
     print(dt.strftime('%Y-%m-%d-%H-%M'))
+
+    try:
+        lmd.invoke(
+            FunctionName=TRIGGERD_LAMDA,
+            InvocationType='Event', Payload=json.dumps({'dt' : dt.strftime('%Y-%m-%d-%H-%M')}))
+    except  Exception as e:
+        print("Error occured when triggering another lambda! {}".format(e))
 
 # lambda 트리거 될 때 dt 오브젝트를 event 요소로 넘겨주면 괜찮을 것 같다. (현재 invoke 된 시간.)
