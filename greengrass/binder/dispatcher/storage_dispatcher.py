aae6316cbce76c28bc936a5c87cd6afab4db0fbc
Kimsehwan96 <sktpghks138@gmail.com>
2021-04-11 23:08:57 +0900

    add storage dispatcher which will convert rawdata into csv

--- greengrass/binder/dispatcher/storage_dispatcher.py
+++ greengrass/binder/dispatcher/storage_dispatcher.py
@@ -1,7 +1,28 @@
-import csv
-import pandas
-
 # TODO:
 # Make csv files
 # Save csv file in local
 # Upload csv file to S3
+import csv
+import pandas
+import os
+from .base_dispatcher import BaseDispatcher
+
+LOCAL_DATA_PATH = '/kpu/rawdata'
+
+
+class StorageDispatcher(BaseDispatcher):
+    def __init__(self):
+        if not os.path.exists(LOCAL_DATA_PATH):
+            os.makedirs(LOCAL_DATA_PATH)
+
+    def write_csv(self, filepath, data):
+        pass
+
+    def copy_to_s3(self, files, objects):
+        pass
+
+    def save_and_upload(self, values):
+        pass
+
+    def relay(self, data: str):
+        pass
