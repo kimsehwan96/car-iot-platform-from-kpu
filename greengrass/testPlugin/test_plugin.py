29b105c977c810b0aa62e6ce2c107bb9faf0a7b2
Kimsehwan96 <sktpghks138@gmail.com>
2021-04-22 01:20:35 +0900

    테스트용 플러그인 리팩터링 (추후 테스트를 위해 가변길이 데이터 적용)

--- greengrass/testPlugin/test_plugin.py
+++ greengrass/testPlugin/test_plugin.py
@@ -3,23 +3,24 @@ from libs.plugin import run_plugin_thread
 from libs.base_plugin import BasePlugin
 
 TEST_FIELDS = [
-        'engine_load',
-        'engine_coolant_temp',
-        'engine_rpm',
-        'vehicle_speed',
-        'maf_sensor',
-        'o2_voltage',
-        'throttle',
-        'short_fuel_trim_bank',
-        'long_fuel_trim_bank',
-        'intake_air_temperature',
-        'engine_runtime',
-        'traveled_distance',
-        'fuel_tank_level',
-        'ambient_air_temperature',
-        'engine_oil_temperature',
-        'transmission_actual_gear'
-    ]
+    'engine_load',
+    'engine_coolant_temp',
+    'engine_rpm',
+    'vehicle_speed',
+    'maf_sensor',
+    'o2_voltage',
+    'throttle',
+    'short_fuel_trim_bank',
+    'long_fuel_trim_bank',
+    'intake_air_temperature',
+    'engine_runtime',
+    'traveled_distance',
+    'fuel_tank_level',
+    'ambient_air_temperature',
+    'engine_oil_temperature',
+    'transmission_actual_gear'
+]
+
 
 def handler(event, context):
     pass
@@ -28,9 +29,11 @@ def handler(event, context):
 class TestPlugin(BasePlugin):
     def __init__(self, fields):
         super().__init__(fields, option={})
+        self._data_len = len(fields)
+
     def collect_data(self):
-        self.data = [randint(0, 100) for x in range(16)]
-        print('this is bufferd data: ', self.data)
+        self.data = [randint(0, 100) for x in range(self._data_len)]
+        print('this is buffered data: ', self.data)
 
 
 tp = TestPlugin(TEST_FIELDS)
